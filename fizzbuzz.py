18c6da52ba4b6c9baa3ce3f00074771057c8d1c0
dukim <sajacall82@naver.com>
2023-09-15 15:15:01 +0900

    feat: print 'fizzbuzz' for 15 multiple
    print 'fizzbuzz', if n is 15 multiple

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,7 +8,9 @@ def do_fizzbuzz():
     나머지 수자는 그대로 출력
     """
     for i in range(1, 20+1):
-        if i%3 == 0:
+        if i%15 == 0:
+            print('fizzbuzz')
+        elif i%3 == 0:
             print('fizz')
         elif i%5 == 0:
             print('buzz')
