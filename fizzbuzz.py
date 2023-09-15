9e85a6527d4aa1a914fcb0c94ff0192affd480ab
dukim <sajacall82@naver.com>
2023-09-15 15:23:35 +0900

    refactor: get endnum from user, and run
    endnum from main: 16

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-def do_fizzbuzz():
+def do_fizzbuzz(num):
     """
     fizzbuzz 기능 수행
     정해진 숫자에 대해 
@@ -7,7 +7,7 @@ def do_fizzbuzz():
     15의 배수는 'fizzbuzz'
     나머지 수자는 그대로 출력
     """
-    for i in range(1, 20+1):
+    for i in range(1, num+1):
         if i%15 == 0:
             print('fizzbuzz')
         elif i%3 == 0:
@@ -18,4 +18,4 @@ def do_fizzbuzz():
             print(i)
 
 if __name__ == '__main__':
-    do_fizzbuzz()
+    do_fizzbuzz(16)
