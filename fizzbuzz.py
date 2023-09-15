33509dac2c0e5c9eda5241a02b81b915c8aced35
dukim <sajacall82@naver.com>
2023-09-15 14:53:19 +0900

    feat: Print fizz
    print fizz if i를 3으로 나눌수 있으면
    Otherwize, print i itself.

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+def do_fizzbuzz():
+    """
+    fizzbuzz 기능 수행
+    정해진 숫자에 대해 
+    3의 배수는 'fizz'
+    5의 배수는 'buzz'
+    15의 배수는 'fizzbuzz'
+    나머지 수자는 그대로 출력
+    """
+    for i in range(1, 20+1):
+        if i%3 == 0:
+            print('fizz')
+        else: 
+            print(i)
+
+if __name__ == '__main__':
+    do_fizzbuzz()
